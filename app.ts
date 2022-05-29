3c0a25c57c15f8ed3bd99d695f60c0e7013eff81
phenphak <phenphak.tae86@gmail.com>
2022-05-29 13:15:19 +0700

    number2

--- app.ts
+++ app.ts
@@ -7,27 +7,27 @@ console.log("-------------------------------------------------------------------
 //template string 1
 console.log("template string 1 test")
 
-//1.ทดลองสร้ำง string อะไรก็ได้มำ 1 ตัวและทดลองใช้ function length ที่หำควำมยำวของ string
+console.log("ทดลองสร้ำง string อะไรก็ได้มำ 1 ตัวและทดลองใช้ function length ที่หำควำมยำวของ string");
 let message=new String("Hello");
 console.log(message.length);
-//2.นำ string จำก ข้อที่ 1 มำ แล้วใช้ function charAt() เพื่อเข้ำถึง element แต่ละตัวของ string
+console.log("นำ string จำก ข้อที่ 1 มำ แล้วใช้ function charAt() เพื่อเข้ำถึง element แต่ละตัวของ string");
 console.log(message.charAt(0));
 console.log(message.charAt(1));
 console.log(message.charAt(2));
 console.log(message.charAt(3));
 console.log(message.charAt(4));
 console.log(message.charAt(5));
-//3.เช่นเดียวกับข้อที่ 2 ลองใช้ในรูปแบบที่เป็น [] (index) เพื่อเข้ำถึง element ของ string เช่นกัน
+console.log("เช่นเดียวกับข้อที่ 2 ลองใช้ในรูปแบบที่เป็น [] (index) เพื่อเข้ำถึง element ของ string เช่นกัน");
 console.log(message[0]);
 console.log(message[1]);
 console.log(message[2]);
 console.log(message[3]);
 console.log(message[4]);
-//4.ลองเปรียบเทียบดูว่ำ charAt() กับ [] (index) มีกำรทำงำนที่เหมือนกันหรือไม่ และมีข้อดีหรือข้อเสียอย่ำงไร
+console.log("ลองเปรียบเทียบดูว่ำ charAt() กับ [] (index) มีกำรทำงำนที่เหมือนกันหรือไม่ และมีข้อดีหรือข้อเสียอย่ำงไร");
 console.log(message.charAt(20));
 console.log(message[20]);
 
-//5.จงนำเอำ string Hello’ และ string world’ มำต่อกัน
+console.log("จงนำเอำ string Hello’ และ string world’ มำต่อกัน");
 let message1='world';
 let message2=`Hello ${message1}`;
 console.log(message2);
@@ -36,11 +36,11 @@ console.log("-------------------------------------------------------")
 
 //template string 2
 console.log("template string 2 test")
-//1.ทดลองเปลี่ยน string microsoft excel’ ให้เป็นตัวใหญ่ทั้งหมด
+console.log("ทดลองเปลี่ยน string microsoft excel’ ให้เป็นตัวใหญ่ทั้งหมด");
 console.log('microsoft excel'.toUpperCase());
-//2.ทดลองเปลี่ยน string GOOGLE AND APPLE’ ให้เป็นตัวเล็กทั้งหมด
+console.log("ทดลองเปลี่ยน string GOOGLE AND APPLE’ ให้เป็นตัวเล็กทั้งหมด");
 console.log('GOOGLE AND APPLE'.toLowerCase())
-//3.ทดลองเปลี่ยน string hello world’ โดยที่ให้ตัวอักษร h และ w เป็นตัวใหญ่
+console.log("ทดลองเปลี่ยน string hello world’ โดยที่ให้ตัวอักษร h และ w เป็นตัวใหญ่");
 let str='hello world';
 str='H'+str[1]+str[2]+str[3]+str[4]+str[5]+'W'+str[7]+str[8]+str[9]+str[10];
 console.log(str);
@@ -48,8 +48,8 @@ console.log(str);
 console.log("------------------------------------------------------------");
 
 //Number
-console.log("Number");
-//1.ทดลองแปลงจำนวนตำมนี้ให้อยู่ในรูปแบบ e
+console.log("Number 1");
+console.log("ทดลองแปลงจำนวนตำมนี้ให้อยู่ในรูปแบบ e ");
 let a= 1000000;
 console.log(a == 1e6);
 let b=10000000;
@@ -60,7 +60,7 @@ let d=0.000012;
 console.log(d==12e-6);
 let e=0.0000001234;
 console.log(e==1234e-10);
-//2.ทดลองแปลงจำนวน e ให้อยู่ในรูปแบบจำนวนปกติ
+console.log("ทดลองแปลงจำนวน e ให้อยู่ในรูปแบบจำนวนปกติ");
 let aa=1e10;
 console.log(aa==10000000000);
 let bb=2e5;
@@ -72,4 +72,39 @@ console.log(dd==0.00000001)
 let ee=12e-5;
 console.log(ee==0.00012);
 
+let num = 1.23456;
+console.log(Math .floor(num * 100) / 100);
+console.log("------------------------------------------------------------");
 
+console.log("Number 2");
+console.log("ทดลองประกาศเลขต่อไปนี้ให้อยู่ในรูปฐาน 8");
+let a2=100;
+console.log(a2.toString(8));
+let b2=111;
+console.log(b2.toString(8)); 
+let c2=55;
+console.log(c2.toString(8));
+let d2=21;
+console.log(d2.toString(8));
+let e2=99;
+console.log(e2.toString(8));
+
+console.log("ทดลองประกาศเลขต่อไปนี้ให้อยู่ในรูปฐาน 16");
+let aa2=100;
+console.log(a2.toString(16));
+let bb2=111;
+console.log(b2.toString(16)); 
+let cc2=55;
+console.log(c2.toString(16));
+let dd2=21;
+console.log(d2.toString(16));
+let ee2=99;
+console.log(e2.toString(16));
+
+console.log("ทดลองใช้ function math.round ในกำรปัดเศษจำนวนต่อไปนี้");
+let aaa2=12.423
+console.log(Math.round(aaa2));
+let bbb2=31313.135;
+console.log(Math.round(bbb2));
+let ccc2=42.809;
+console.log(Math.round(ccc2));
--- out/app.js
+++ out/app.js
@@ -4,44 +4,44 @@ console.log(text);
 console.log("---------------------------------------------------------------------------------------");
 //template string 1
 console.log("template string 1 test");
-//1.ทดลองสร้ำง string อะไรก็ได้มำ 1 ตัวและทดลองใช้ function length ที่หำควำมยำวของ string
+console.log("ทดลองสร้ำง string อะไรก็ได้มำ 1 ตัวและทดลองใช้ function length ที่หำควำมยำวของ string");
 var message = new String("Hello");
 console.log(message.length);
-//2.นำ string จำก ข้อที่ 1 มำ แล้วใช้ function charAt() เพื่อเข้ำถึง element แต่ละตัวของ string
+console.log("นำ string จำก ข้อที่ 1 มำ แล้วใช้ function charAt() เพื่อเข้ำถึง element แต่ละตัวของ string");
 console.log(message.charAt(0));
 console.log(message.charAt(1));
 console.log(message.charAt(2));
 console.log(message.charAt(3));
 console.log(message.charAt(4));
 console.log(message.charAt(5));
-//3.เช่นเดียวกับข้อที่ 2 ลองใช้ในรูปแบบที่เป็น [] (index) เพื่อเข้ำถึง element ของ string เช่นกัน
+console.log("เช่นเดียวกับข้อที่ 2 ลองใช้ในรูปแบบที่เป็น [] (index) เพื่อเข้ำถึง element ของ string เช่นกัน");
 console.log(message[0]);
 console.log(message[1]);
 console.log(message[2]);
 console.log(message[3]);
 console.log(message[4]);
-//4.ลองเปรียบเทียบดูว่ำ charAt() กับ [] (index) มีกำรทำงำนที่เหมือนกันหรือไม่ และมีข้อดีหรือข้อเสียอย่ำงไร
+console.log("ลองเปรียบเทียบดูว่ำ charAt() กับ [] (index) มีกำรทำงำนที่เหมือนกันหรือไม่ และมีข้อดีหรือข้อเสียอย่ำงไร");
 console.log(message.charAt(20));
 console.log(message[20]);
-//5.จงนำเอำ string Hello’ และ string world’ มำต่อกัน
+console.log("จงนำเอำ string Hello’ และ string world’ มำต่อกัน");
 var message1 = 'world';
 var message2 = "Hello ".concat(message1);
 console.log(message2);
 console.log("-------------------------------------------------------");
 //template string 2
 console.log("template string 2 test");
-//1.ทดลองเปลี่ยน string microsoft excel’ ให้เป็นตัวใหญ่ทั้งหมด
+console.log("ทดลองเปลี่ยน string microsoft excel’ ให้เป็นตัวใหญ่ทั้งหมด");
 console.log('microsoft excel'.toUpperCase());
-//2.ทดลองเปลี่ยน string GOOGLE AND APPLE’ ให้เป็นตัวเล็กทั้งหมด
+console.log("ทดลองเปลี่ยน string GOOGLE AND APPLE’ ให้เป็นตัวเล็กทั้งหมด");
 console.log('GOOGLE AND APPLE'.toLowerCase());
-//3.ทดลองเปลี่ยน string hello world’ โดยที่ให้ตัวอักษร h และ w เป็นตัวใหญ่
+console.log("ทดลองเปลี่ยน string hello world’ โดยที่ให้ตัวอักษร h และ w เป็นตัวใหญ่");
 var str = 'hello world';
 str = 'H' + str[1] + str[2] + str[3] + str[4] + str[5] + 'W' + str[7] + str[8] + str[9] + str[10];
 console.log(str);
 console.log("------------------------------------------------------------");
 //Number
-console.log("Number");
-//1.ทดลองแปลงจำนวนตำมนี้ให้อยู่ในรูปแบบ e
+console.log("Number 1");
+console.log("ทดลองแปลงจำนวนตำมนี้ให้อยู่ในรูปแบบ e ");
 var a = 1000000;
 console.log(a == 1e6);
 var b = 10000000;
@@ -52,7 +52,7 @@ var d = 0.000012;
 console.log(d == 12e-6);
 var e = 0.0000001234;
 console.log(e == 1234e-10);
-//2.ทดลองแปลงจำนวน e ให้อยู่ในรูปแบบจำนวนปกติ
+console.log("ทดลองแปลงจำนวน e ให้อยู่ในรูปแบบจำนวนปกติ");
 var aa = 1e10;
 console.log(aa == 10000000000);
 var bb = 2e5;
@@ -63,4 +63,37 @@ var dd = 1e-8;
 console.log(dd == 0.00000001);
 var ee = 12e-5;
 console.log(ee == 0.00012);
+var num = 1.23456;
+console.log(Math.floor(num * 100) / 100);
+console.log("------------------------------------------------------------");
+console.log("Number 2");
+console.log("ทดลองประกาศเลขต่อไปนี้ให้อยู่ในรูปฐาน 8");
+var a2 = 100;
+console.log(a2.toString(8));
+var b2 = 111;
+console.log(b2.toString(8));
+var c2 = 55;
+console.log(c2.toString(8));
+var d2 = 21;
+console.log(d2.toString(8));
+var e2 = 99;
+console.log(e2.toString(8));
+console.log("ทดลองประกาศเลขต่อไปนี้ให้อยู่ในรูปฐาน 16");
+var aa2 = 100;
+console.log(a2.toString(16));
+var bb2 = 111;
+console.log(b2.toString(16));
+var cc2 = 55;
+console.log(c2.toString(16));
+var dd2 = 21;
+console.log(d2.toString(16));
+var ee2 = 99;
+console.log(e2.toString(16));
+console.log("ทดลองใช้ function math.round ในกำรปัดเศษจำนวนต่อไปนี้");
+var aaa2 = 12.423;
+console.log(Math.round(aaa2));
+var bbb2 = 31313.135;
+console.log(Math.round(bbb2));
+var ccc2 = 42.809;
+console.log(Math.round(ccc2));
 //# sourceMappingURL=app.js.map
